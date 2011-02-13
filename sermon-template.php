61b198e6f97ff1e4dce9fae105eb9d7bf7256e08
fonglh <fonglh@users.noreply.github.com>
2011-02-13 17:03:43 +0000

    modified template to work with custom post type archives plugin

--- sermon-template.php
+++ sermon-template.php
@@ -11,8 +11,19 @@
 		<div id="container">
 			<div id="content">
 <?php 
+global $wp_query;
+$existing = $wp_query->query;
 
-query_posts(array('post_type'=>'sermon','posts_per_page'=>5,'paged'=>get_query_var('paged')));
+//if on sermons display page, change the whole query
+if(isset($existing['pagename']))
+	$args = array('post_type'=>'sermon','posts_per_page'=>5,'paged'=>get_query_var('paged'));
+//if it's a date archive, add the parameters on to the existing query with the month/yr info
+//there should only be 4 sermons/mth, so no need to limit the posts per page
+//in WP 3.0.5, pagination doesn't work for custom post type archives even with the plugin
+else
+	$args = array_merge($wp_query->query,array('post_type'=>'sermon','paged'=>get_query_var('paged')));
+print_r($args);
+query_posts($args);
 ?>
 
 <?php
