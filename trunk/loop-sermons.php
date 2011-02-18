2e565eec60ecf8676dd9564b08322706b29e0a65
fonglh <fonglh@be4bdf85-7ca7-4b34-af36-d026590cb350>
2011-02-18 13:05:20 +0000

    took out fb like button code as a template and inserted it into the sermon page loop

--- trunk/loop-sermons.php
+++ trunk/loop-sermons.php
@@ -67,6 +67,7 @@
 			</div><!-- .entry-content -->
 
 			<div class="entry-utility">
+				<?php get_template_part('fblike'); ?>
 				<?php if ( count( get_the_category() ) ) : ?>
 					<span class="cat-links">
 						<?php printf( __( '<span class="%1$s">Posted in</span> %2$s', 'twentyten' ), 'entry-utility-prep entry-utility-prep-cat-links', get_the_category_list( ', ' ) ); ?>
