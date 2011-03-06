66261756b920e7dc547999d6e2c58bf05f5fc505
fonglh <fonglh@users.noreply.github.com>
2011-03-06 14:26:03 +0000

    removing accidental tag in release-0.5

--- trunk/fblike.php
+++
@@ -1,4 +0,0 @@
-	<!-- code for facebook like button 
-	     Just for this iframe, the style is defined inline -->
-	<iframe src="http://www.facebook.com/plugins/like.php?href=<?php echo urlencode(get_permalink($post->ID)); ?>&amp;layout=standard&amp;show_faces=false&amp;width=450&amp;action=like&amp;colorscheme=light" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:450px; height:25px; display:block; clear:both;"></iframe>
-
