e1022b2ceeac6956cd521b2c724e256816dd64d1
fonglh <fonglh@be4bdf85-7ca7-4b34-af36-d026590cb350>
2011-04-19 16:08:13 +0000

    tagging release 1.2. custom sermon upload. translations for descriptions.

---
+++ tags/releases-1.2/taxonomy-speaker.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying sermons from the speaker taxonomy.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+				<?php //$speaker_name = get_terms('speaker','fields=names&slug='.$term); 
+				$term = get_term_by('slug',get_query_var('term'),get_query_var('taxonomy'));?>
+				<h1 class="page-title"><?php
+				printf( __( 'Sermons by %s', 'twentyten' ), '<span>' . $term->name . '</span>' );
+				?></h1>
+
+<?php
+/* Run the loop for the tag archive to output the posts
+ * If you want to overload this in a child theme then include a file
+ * called loop-tag.php and that will be used instead.
+ */
+ get_template_part( 'loop', 'sermons' );
+?>
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
