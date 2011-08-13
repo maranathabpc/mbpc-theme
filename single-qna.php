62f72a29bdb26160584790bcb838d503287e1611
fonglh <fonglh@users.noreply.github.com>
2011-08-13 05:33:07 +0000

    added custom post type 'qna' for question and answer PDF files. modelled after the newsletter post type. added custom page templates, loop, single and archive files to support this. also had to modify sidebar.php to support yet another widget area

---
+++ single-qna.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The Template for displaying all single newsletters.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+
+			<?php get_template_part('loop','qnas');?>
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
