96d4b8db9b1244664a9f273d2cd1065f6a928143
fonglh <fonglh@be4bdf85-7ca7-4b34-af36-d026590cb350>
2011-10-08 11:21:09 +0000

    tagging releases-1.6. new page template for links. minor css change to align 'Weekly Activities' text to the left

---
+++ tags/releases-1.6/scripts/jquery-accordion-init.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function() {
+				jQuery("#accordion").accordion({
+					autoHeight: false,
+					collapsible: true
+				});
+				jQuery("#accordion-home").accordion({
+					autoHeight: false,
+					collapsible: true,
+					active: false
+				});
+			});
