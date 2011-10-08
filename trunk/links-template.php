458311af3efd705afbbd342301cdd82ad26d679c
fonglh <fonglh@be4bdf85-7ca7-4b34-af36-d026590cb350>
2011-10-08 11:19:07 +0000

    added new page template for links

---
+++ trunk/links-template.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template Name: Links page 
+ *
+ * Selectable from a dropdown menu on the edit page screen.
+ * For listing the links entered in the Links admin page.
+ */
+?>
+
+<?php get_header(); ?>
+
+		<div id="container">
+			<div id="content">
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php if ( is_front_page() ) { ?>
+						<h2 class="entry-title"><?php the_title(); ?></h2>
+					<?php } else { ?>
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					<?php } ?>
+
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_list_bookmarks(); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="edit-link">', '</span>' ); ?>
+					</div><!-- .entry-content -->
+				</div><!-- #post-## -->
+
+				<?php comments_template( '', true ); ?>
+
+<?php endwhile; ?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
